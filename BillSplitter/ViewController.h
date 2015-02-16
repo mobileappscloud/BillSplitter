08f0b3d24b50126403d9a5f2884ee1cd07aa2987
Faisal_Syed <doc92606@gmail.com>
2015-02-15 19:14:37 -0800

    Initial Commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Mac on 2/15/15.
+//  Copyright (c) 2015 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
